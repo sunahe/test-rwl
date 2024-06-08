d169ad592bb072a22e037ae1200d57027cbbd0e8
Alexander Omelchuk <CubesFriend@gmail.com>
2024-06-08 21:26:51 +0200

    Add SpinWait results

---
+++ RWL_SpinWait.cs
@@ -0,0 +1,41 @@
+class RWL_SpinWait
+{
+    private volatile int readerCount;
+
+    public ReadLockHandle GetReadLock()
+    {
+        while (true)
+        {
+            var spinWait = new SpinWait();
+            var cnt = readerCount;
+            if (cnt < int.MaxValue && Interlocked.CompareExchange(ref readerCount, cnt + 1, cnt) == cnt)
+                return new ReadLockHandle(this);
+            spinWait.SpinOnce();
+        }
+    }
+
+    public WriteLockHandle GetWriteLock()
+    {
+        while (true)
+        {
+            var spinWait = new SpinWait();
+            if (Interlocked.CompareExchange(ref readerCount, int.MaxValue, 0) == 0)
+                return new WriteLockHandle(this);
+            spinWait.SpinOnce(10);
+        }
+    }
+
+    public readonly struct ReadLockHandle(RWL_SpinWait owner) : IDisposable
+    {
+        public readonly void Dispose()
+            => Interlocked.Decrement(ref owner.readerCount);
+    }
+
+    public readonly struct WriteLockHandle(RWL_SpinWait owner) : IDisposable
+    {
+        public readonly void Dispose()
+        {
+            owner.readerCount = 0;
+        }
+    }
+}
